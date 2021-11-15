ba33ec610aa0d6017553f85e4befb153cb335ab8
itspaulyg <pgierl@nd.edu>
2021-11-14 19:48:41 -0500

    added pub/sub for tbot/car and basic car movement

--- src/follow.py
+++ src/follow.py
@@ -3,29 +3,41 @@
 import rospy
 from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
 from states.utils import straight, turn_circle
 
-'''
-call_back message from turtlebot's post
 
 def call_back(msg):
 
-# TODO read the message data and determine where to move
-# use util functions to move car 
+    move_topic = rospy.get_param("~move_topic", "/car/mux/ackermann_cmd_mux/input/navigation")
+    move_pub = rospy.Publisher("move_topic", AckermannDriveStamped, queue_size=1)
 
-'''
+    movement = msg.data
+
+    # use util functions to move car
+    if movement == "Drive":
+        straight(move_pub, 1, 1)
+    elif movement == "Left":
+        turn_circle(move_pub, 0.4, 1, 1, 0.25)
+    elif movement == "Right":
+        turn_circle(move_pub, 0.4, 0, 1, 0.25)
+    elif movement == "Stop":
+        move_pub(AckermannDriveStamped())
+    else:
+        rospy.loginfo("Invalid message received")
+
 
 def main():
 
     rospy.init_node("follow")
 
-    # TODO add subscriber to turtlebot topic 
+    rospy.Subscriber("comms", String, call_back)
 
     move_topic = rospy.get_param("~move_topic", "/car/mux/ackermann_cmd_mux/input/navigation")
     move_pub = rospy.Publisher("move_topic", AckermannDriveStamped, queue_size=1)
 
     while not rospy.is_shutdown():
-        rospy.spin()
+        continue
     move_pub(AckermannDriveStamped())
 
 
--- src/navigate.py
+++ src/navigate.py
@@ -4,12 +4,15 @@ import rospy
 from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 
 SCAN_TOPIC="/scan"
 MOVE_TOPIC="cmd_vel"
 
 
 def call_back(msg)
+
+    comms_pub = rospy.Publisher("comms", String, queue_size=10)
 
     rospy.loginfo(msg.ranges)
     rospy.loginfo(msg.range_min)
@@ -18,14 +21,18 @@ def call_back(msg)
     # TODO check for obstacle based on msg.ranges
     # what ranges[i] is front of turtlebot 
 
-    # TODO publish message to car topic
-    # best way to send message?
-
     # TODO publish movement for turtlebot
     # if no wall 
     # go forward 
     # else
     # turn left or right
+
+    # TODO publish message to car topic based on obstacle/tbot movement
+    # comms_pub.Publish("Drive")
+    # comms_pub.Publish("Stop")
+    # comms_pub.Publish("Left")
+    # comms_pub.Publish("Right")
+
 
 def main():
 
